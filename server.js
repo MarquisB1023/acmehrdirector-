4d5783af7441f96b5a44584f4f1c610b98984e5d
marquis brunbdage <marquisbrunbdage@marquiss-MacBook-Pro.local>
2024-03-05 00:12:13 -0500

    post get tested for department

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const init = () => {
       id SERIAL PRIMARY KEY,
       name VARCHAR(255) NOT NULL
     );
-  
+
     DROP TABLE IF EXISTS employees;
     CREATE TABLE employees (
       id SERIAL PRIMARY KEY,
@@ -28,16 +28,16 @@ const init = () => {
       updated_at TIMESTAMP DEFAULT NOW(),
       department_id INTEGER REFERENCES departments(id) NOT NULL
     );
-  
+
     INSERT INTO departments (name) VALUES ('information technology');
     INSERT INTO departments (name) VALUES ('human resources');
     INSERT INTO departments (name) VALUES ('production');
     INSERT INTO departments (name) VALUES ('marketing');
-  
-    INSERT INTO employees (name, department_id) VALUES ('Alica',1);
-    INSERT INTO employees (name, department_id) VALUES ('Shannah',2);
-    INSERT INTO employees (name, department_id) VALUES ('Lenard',3);
-    INSERT INTO employees (name, department_id) VALUES ('Terrance',4);
+
+    INSERT INTO employees (name, department_id) SELECT 'Alica', id FROM departments WHERE name = 'information technology';
+    INSERT INTO employees (name, department_id) SELECT 'Shannah', id FROM departments WHERE name = 'human resources';
+    INSERT INTO employees (name, department_id) SELECT 'Lenard', id FROM departments WHERE name = 'production';
+    INSERT INTO employees (name, department_id) SELECT 'Terrance', id FROM departments WHERE name = 'marketing';
   `;
   const response = client.query(SQL);
   app.listen(port, () => {
@@ -64,10 +64,10 @@ app.get("/api/departments", async (req, res, next) => {
 app.get("/api/employees", async (req, res, next) => {
   try {
     const SQL = `
-        SELECT * FROM employees;
+        SELECT  id, name, department_id  FROM employees;
         `;
     const response = await client.query(SQL);
-    res.send(response.rows[0], response.rows);
+    res.send(response.rows);
   } catch (error) {
     next(error);
   }
@@ -77,8 +77,7 @@ app.post("/api/employees", async (req, res, next) => {
   try {
     const { name, department_id } = req.body;
     const SQL = `
-        INSERT INTO employees(name, department_id)
-        VALUES($1, $2, $3, (SELECT id from departments where name = $4))
+        INSERT INTO employees(name, department_SELECT VALUES($1, id FROM despartments WHERE name =, $2, $3, (SELECT id from departments where name = $4))
         RETURNING *;
         `;
     const response = await client.query(SQL, [name, department_id]);
@@ -92,13 +91,14 @@ app.put("/api/employees/:id", async (req, res, next) => {
   try {
     const SQL = `
         UPDATE employees
-        SET txt=$1, rannking=$2, name =$3, department_id = (SELECT id from departments where name = $4))
-       where id = $5 
+        SET name=$1, department_id =$2
+       where id = $3
        RETURNING *;
         `;
-    const response = await client.query(
-      SQL[(req.body.name, req.body.departments)]
-    );
+    const response = await client.query(SQL, [
+      req.body.name,
+      req.body.departments,
+    ]);
     res.send(response.rows[0], response.rows);
   } catch (error) {
     next(error);
@@ -110,7 +110,7 @@ app.delete("/api/employees/:id", async (req, res, next) => {
     const { id } = req.params;
     const SQL = `
         DELETE FROM employees
-        
+
        where id = $1;
         `;
     const response = await client.query(SQL, [id]);
